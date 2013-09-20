4595b2066098b1f422a701feb0c4e1f76f398762
joe <joe@localhost>
2013-09-19 20:30:30 -0500

    bound buffer

---
+++ threads/threads/BoundBuffer.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace threads
+{
+	public class BoundBuffer<T> {
+		private readonly Semaphore _full;
+		private readonly Semaphore _empty;
+		private readonly Semaphore _lock;
+		private readonly Queue<T> _queue;
+
+		public BoundBuffer (int maxCount) {
+			_empty = new Semaphore (maxCount, maxCount);
+			_full = new Semaphore (0, maxCount);
+			_lock = new Semaphore (1, 1);
+			_queue = new Queue<T> ();
+		}
+
+		public void Enqueue(T item) {
+			_empty.WaitOne ();
+			_lock.WaitOne ();
+			_queue.Enqueue (item);
+			_lock.Release (1);
+			_full.Release (1);
+		}
+
+		public T Dequeue() {
+			_full.WaitOne ();
+			_lock.WaitOne ();
+			var item = _queue.Dequeue ();
+			_lock.Release(1);
+			_empty.Release(1);
+			return item;
+		}
+	}
+}
+
