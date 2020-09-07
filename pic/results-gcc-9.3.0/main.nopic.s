b41c2ea711b341b801fd977aa89a25ded35bafde
George K. Thiruvathukal <gkt@cs.luc.edu>
2020-09-07 09:25:20 -0500

    save results for gcc 9.3.0 (used for book)

---
+++ pic/results-gcc-9.3.0/main.nopic.s
@@ -0,0 +1,60 @@
+	.file	"main.c"
+	.text
+	.globl	msg
+	.section	.rodata
+.LC0:
+	.string	"abcdefg"
+	.data
+	.align 8
+	.type	msg, @object
+	.size	msg, 8
+msg:
+	.quad	.LC0
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movq	%rsi, -32(%rbp)
+	movq	msg(%rip), %rax
+	addq	$1, %rax
+	movq	%rax, -8(%rbp)
+	movq	-8(%rbp), %rax
+	movl	$1, %edx
+	movq	%rax, %rsi
+	movl	$1, %edi
+	call	write
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.3.0-10ubuntu2) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
