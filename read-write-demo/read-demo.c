95eea22ebf19b2dd1e73c049c664583c46dd5c90
George K. Thiruvathukal <gkt@cs.luc.edu>
2020-09-18 09:05:45 -0500

    put read and write examples into a program form so students can try it

---
+++ read-write-demo/read-demo.c
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+
+    int fd = open("file", O_RDONLY, 0666);
+    size_t length;
+    char buffer[5];
+    while((length = read(fd, &buffer[0], 5)) != 0) {
+        write(1, &buffer[0], length);
+    }
+    close(fd);
+
+}
---
+++ read-write-demo/write-demo.c
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    const char *data = "foobar";
+    int fd = open("file", O_CREAT | O_TRUNC | O_RDWR, 0666);
+    size_t length = strlen(data), offset = 0;
+    while(length > 0) {
+        size_t written = write(fd, data + offset, length);
+        offset += written;
+        length -= written;
+    }
+    close(fd);
+}
