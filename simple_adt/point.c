dddf66aadaa0228e24e7766eeb8a381e7f5825e2
George K. Thiruvathukal <gkt@cs.luc.edu>
2020-09-25 08:38:07 -0500

    some initial notes

---
+++ simple_adt/point.c
@@ -0,0 +1,41 @@
+#include "point.h"
+
+#include <math.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void point_new(point_t* point) {
+   point->x = 0;
+   point->y = 0;
+}
+
+void point_init(point_t* point, double x, double y) {
+   point->x = x;
+   point->y = y;
+}
+
+void point_add(point_t* point, point_t* another) {
+   point->x += another->x;
+   point->y += another->y;
+}
+
+double point_distance(point_t* point, point_t* another) {
+   double x0 = point->x, y0 = point->y;
+   double x1 = another->x, y1 = another->y;
+   return sqrt( pow(x1 - x0, 2) + pow(y1 - y0, 2));
+}
+
+double point_x(point_t* point) {
+   return point->x;
+}
+   
+double point_y(point_t* point) {
+   return point->y;
+}
+
+extern void point_print(point_t* point, FILE* stream) {
+   fprintf(stream, "point(%lf, %lf)", point->x, point->y);
+}
+
---
+++ simple_adt/point.h
@@ -0,0 +1,29 @@
+/*
+ * This example is often seen in OOP books. 
+ * It is to represent a 2D point. We start with double x, y.
+ */
+
+#ifndef _POINT_H_
+#define _POINT_H_
+
+#include <stdio.h>
+
+typedef struct _point_t {
+    double x, y;
+} point_t;
+
+extern void point_new(point_t* point);;
+
+extern void point_init(point_t* point, double x, double y);
+
+extern void point_add(point_t* point, point_t* another);
+
+extern double point_distance(point_t* point, point_t* another);
+
+extern double point_x(point_t* point);
+
+extern double point_y(point_t* point);
+
+extern void point_print(point_t* point, FILE* stream);
+
+#endif
