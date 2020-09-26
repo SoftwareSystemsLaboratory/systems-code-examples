fbc2a66dda227ef1d4afaa6ca0fac75cb2cb21e4
George K. Thiruvathukal <gkt@cs.luc.edu>
2020-09-26 12:29:28 -0500

    show how to build your own API around TAILQ

---
+++ std-queue/wordlist-demo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "wordlist.h"
+
+int main(int argc, char* argv[]) {
+
+   struct wordlist_t wordlist;
+   wordlist_init(&wordlist);
+
+   /* add each argument (after 0) to queue; just keep pointer */
+   for (int i=1; i < argc; i++) {
+     wordlist_add(&wordlist, argv[i]);
+   }
+
+   wordlist_print(&wordlist);
+   wordlist_delete(&wordlist);
+}
---
+++ std-queue/wordlist.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/queue.h>
+
+#ifndef WORDLIST_TAILQ_H
+#define WORDLIST_TAILQ_H
+
+TAILQ_HEAD(wordlist_t, wordentry_t);
+
+struct wordentry_t {
+  char* text;
+  TAILQ_ENTRY(wordentry_t) entries;
+};
+
+extern void wordlist_add(struct wordlist_t* head_ptr, char* word);
+
+extern void wordlist_print(struct wordlist_t* head_ptr);
+
+extern void wordlist_delete(struct wordlist_t* head_ptr);
+
+extern void wordlist_init(struct wordlist_t* head_ptr);
+
+#endif
