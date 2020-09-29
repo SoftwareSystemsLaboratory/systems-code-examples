9321988e965ef208ba57a201853f3a70754c4509
George K. Thiruvathukal <gkt@cs.luc.edu>
2020-09-29 09:02:21 -0500

    some updates to highlight the pedagogical ideas

--- qsort-demo/try-qsort.c
+++ qsort-demo/try-qsort.c
@@ -40,12 +40,17 @@ static int qsort_cmp_wordcount(const void *p1, const void *p2)
 
 int main(int argc, char *argv[])
 {
+    /* these fixed size arrays are being used to populate a dynamic one */
     const char* some_words[] = { "a", "b", "c", "d", "e", "beta", "gamma", "epsilon", "delta" };
     int some_counts[] = { 8, 6, 7, 5, 3, -5, 25, 10, 3, 13, 17 };
 
+    /* shows how to find the number of elements in an array without hard-coding */
     const int some_words_size = sizeof(some_words) / sizeof(const char*);
     const int some_counts_size = sizeof(some_counts) / sizeof(int);
     const int items = min(some_words_size, some_counts_size);
+
+    /* use this to create dynamic array of words and their counts */
+    /* note: this is not intended to be a wordcount program. There's intentionally no I/O */
 
     wordcount_info_t **word_counts = (wordcount_info_t **) malloc(items * sizeof(wordcount_info_t*));
     for (int i=0; i < items; i++) {
@@ -54,28 +59,31 @@ int main(int argc, char *argv[])
         word_counts[i]->count = some_counts[i];
     }
 
-    printf("Original word list\n");
+    printf("Word Array\n");
     for (int i=0; i < items; i++) {
         printf("word %s: %d\n", word_counts[i]->word, word_counts[i]->count);
     }
+    printf("\n");
 
+    /* perform general qsort() using library function - sort by the wordcount field, descending */
     qsort(word_counts, items, sizeof(wordcount_info_t*), qsort_cmp_wordcount);
 
-    printf("Sorted word list\n");
+    printf("Sorted Word Array by Count\n");
     for (int i=0; i < items; i++) {
-        printf("word %s: %d\n", word_counts[i]->word, word_counts[i]->count);
+        printf("{ word : '%s',  count : %d }\n", word_counts[i]->word, word_counts[i]->count);
     }
+    printf("\n");
 
-    printf("Search results\n");
+    /* use general bsearch() using library function -  requires data to be sorted */
+    /* because we use the same comparison function for qsort() and bsearch(), we can do a bsearch() on data in ascending or descending order */
+    printf("Search Results\n");
     for (int count=0; count < 15; count++) {
         wordcount_info_t *search_key = (wordcount_info_t *)malloc(sizeof(wordcount_info_t));
         search_key->word = "any";
         search_key->count = count;
         wordcount_info_t **item = (wordcount_info_t **) bsearch (&search_key, word_counts, items, sizeof (wordcount_info_t*), qsort_cmp_wordcount);
         if (item != NULL)  {
-            printf("Found count %d for word %s\n", (*item)->count, (*item)->word);;
-        } else {
-            printf("Not found count value %d \n", count);
+            printf("{ word : '%s' : count : %d }\n", (*item)->word, (*item)->count);;
         }
         free(search_key);
     }
