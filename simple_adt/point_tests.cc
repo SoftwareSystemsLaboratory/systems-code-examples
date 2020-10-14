9d8f8ea56f36403b1236fa6444b955607f08c1fc
George K. Thiruvathukal <gkt@cs.luc.edu>
2020-10-13 21:39:09 -0500

    show more than one test

--- simple_adt/point_tests.cc
+++ simple_adt/point_tests.cc
@@ -4,11 +4,42 @@
 
 #include "point.hh"
 
-TEST(PointTest, Basics) {
+TEST(PointTest, Initialization) {
     point_t p1, p2, p3;
+
     point_init(&p1, 3, 0);
     point_init(&p2, 0, 4);
     point_new(&p3);
+
+    ASSERT_FLOAT_EQ( point_x(&p1), 3.0);
+    ASSERT_FLOAT_EQ( point_y(&p1), 0.0);
+
+    ASSERT_FLOAT_EQ( point_x(&p2), 0.0);
+    ASSERT_FLOAT_EQ( point_y(&p2), 4.0);
+
+    ASSERT_FLOAT_EQ( point_x(&p3), 0.0);
+    ASSERT_FLOAT_EQ( point_y(&p3), 0.0);
+
+}
+
+
+TEST(PointTest, Add) {
+    point_t p1, p2, p3;
+
+    point_new(&p3);
+    point_init(&p1, 3, 0);
+    point_init(&p2, 0, 4);
+    point_add(&p3, &p1);
+    point_add(&p3, &p2);
+    ASSERT_FLOAT_EQ( point_x(&p3), 3.0);
+    ASSERT_FLOAT_EQ( point_y(&p3), 4.0);
+}
+
+TEST(PointTest, Distance) {
+    point_t p1, p2, p3;
+
+    point_init(&p1, 3, 0);
+    point_init(&p2, 0, 4);
     ASSERT_FLOAT_EQ( point_distance(&p1, &p2), 5.0);
 }
 
