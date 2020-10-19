83d0170ac5f46737cd129c55f2787f5e582953ad
George K. Thiruvathukal <gkt@cs.luc.edu>
2020-10-18 23:36:24 -0500

    a bit of reformatting/cleanup

--- bounded-buffer/bounded-buffer.c
+++ bounded-buffer/bounded-buffer.c
@@ -15,56 +15,56 @@
 #define NUM_THREADS ((NUM_SUPPLIERS) + (NUM_CONSUMERS))
 
 typedef struct {
-  int value;
+    int value;
 } entry_t;
 
 typedef struct {
-  int size;
-  entry_t** entries;
-  int head;
-  int tail;
-  pthread_mutex_t lock;
-  pthread_cond_t has_space;
-  pthread_cond_t has_items;
+    int size;
+    entry_t** entries;
+    int head;
+    int tail;
+    pthread_mutex_t lock;
+    pthread_cond_t has_space;
+    pthread_cond_t has_items;
 } bounded_buffer_t;
 
 void bounded_buffer_init(bounded_buffer_t* bb, int size) {
-  bb->entries = (entry_t**) malloc(size * sizeof(entry_t*));
-  for (int i=0; i < size; i++)
-    bb->entries[size] = NULL;
-  bb->size = size;
-  bb->head = bb->tail = 0;
-  pthread_mutex_init(&bb->lock, NULL);
-  pthread_cond_init (&bb->has_space, NULL);
-  pthread_cond_init (&bb->has_items, NULL);
+    bb->entries = (entry_t**) malloc(size * sizeof(entry_t*));
+    for (int i=0; i < size; i++)
+        bb->entries[size] = NULL;
+    bb->size = size;
+    bb->head = bb->tail = 0;
+    pthread_mutex_init(&bb->lock, NULL);
+    pthread_cond_init (&bb->has_space, NULL);
+    pthread_cond_init (&bb->has_items, NULL);
 }
 
 void bounded_buffer_put(bounded_buffer_t* bb, entry_t* item)  {
-  pthread_mutex_lock(&bb->lock);
+    pthread_mutex_lock(&bb->lock);
 
-  while (bb->tail - bb->head >= bb->size) {
-    pthread_cond_wait(&bb->has_space, &bb->lock);
-  }
+    while (bb->tail - bb->head >= bb->size) {
+        pthread_cond_wait(&bb->has_space, &bb->lock);
+    }
 
-  bb->entries[bb->tail++ % bb->size] = item;
-  pthread_cond_signal(&bb->has_items);
+    bb->entries[bb->tail++ % bb->size] = item;
+    pthread_cond_signal(&bb->has_items);
 
-  pthread_mutex_unlock(&bb->lock);
+    pthread_mutex_unlock(&bb->lock);
 }
 
 entry_t* bounded_buffer_get(bounded_buffer_t* bb)  {
-  pthread_mutex_lock(&bb->lock);
+    pthread_mutex_lock(&bb->lock);
 
-  while (bb->tail == bb->head) {
-     pthread_cond_wait(&bb->has_items, &bb->lock);
-  }
+    while (bb->tail == bb->head) {
+        pthread_cond_wait(&bb->has_items, &bb->lock);
+    }
 
-  entry_t* entry = bb->entries[bb->head % bb->size];
-  bb->entries[bb->head++ % bb->size] = NULL;
-  pthread_cond_signal(&bb->has_space);
+    entry_t* entry = bb->entries[bb->head % bb->size];
+    bb->entries[bb->head++ % bb->size] = NULL;
+    pthread_cond_signal(&bb->has_space);
 
-  pthread_mutex_unlock(&bb->lock);
-  return entry;
+    pthread_mutex_unlock(&bb->lock);
+    return entry;
 }
 
 void bounded_buffer_cleanup(bounded_buffer_t* bb) {
@@ -110,11 +110,11 @@ int main (int argc, char *argv[])
 
     /* TODO: Consider making thread-specific data include supplier/consumer IDs */
     int thread_count = 0;
-    for (int i=0;i < NUM_SUPPLIERS; i++)
-       pthread_create(&threads[thread_count++], &attr, supplier, (void *)&bb);
+    for (int i=0; i < NUM_SUPPLIERS; i++)
+        pthread_create(&threads[thread_count++], &attr, supplier, (void *)&bb);
 
-    for (int i=0;i < NUM_CONSUMERS; i++)
-       pthread_create(&threads[thread_count++], &attr, consumer, (void *)&bb);
+    for (int i=0; i < NUM_CONSUMERS; i++)
+        pthread_create(&threads[thread_count++], &attr, consumer, (void *)&bb);
 
     for (int i=0; i<NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
