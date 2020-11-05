08a76f87ab3ecff0b41ddbdc1396ed2fafe3d0ae
George K. Thiruvathukal <gkt@cs.luc.edu>
2020-11-05 17:14:02 -0600

    update some definitions for info/debug purposes

--- bounded-buffer/bbuffer.h
+++ bounded-buffer/bbuffer.h
@@ -1,6 +1,12 @@
 #ifndef _BBUFFER_H_
 #define _BBUFFER_H_
 
+#include <stdio.h>
+
+#define DEBUG(fmt, ...) do { fprintf(stderr, fmt, ## __VA_ARGS__); } while (0)
+
+#define INFO(fmt, ...) do { fprintf(stdout, fmt, ## __VA_ARGS__); } while (0)
+
 #include <pthread.h>
 
 typedef struct {
@@ -20,5 +26,6 @@ typedef struct {
 extern void bounded_buffer_init(bounded_buffer_t* bb, int size);
 extern void bounded_buffer_put(bounded_buffer_t* bb, entry_t* item);
 extern entry_t* bounded_buffer_get(bounded_buffer_t* bb);
+extern int bounded_buffer_size(bounded_buffer_t* bb);
 
 #endif
