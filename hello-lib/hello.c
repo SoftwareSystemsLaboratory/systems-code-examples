e3638750eb40f6096812e8cc5680806b6465a2a6
George K. Thiruvathukal <gkt@cs.luc.edu>
2021-09-25 23:50:34 -0500

    add MVA of a module with internal library dependency

---
+++ hello-lib/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "hello.h"
+
+void say_hi(char* person) {
+  printf("Hello, %s\n", person);
+}
+
---
+++ hello-lib/hello.h
@@ -0,0 +1,8 @@
+#ifndef _HELLO_H_
+
+#define _HELLO_H_
+
+extern void say_hi(char* person);
+
+#endif
+
