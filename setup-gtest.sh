8ecd894637812d812935b6da1a38e115da495207
George K. Thiruvathukal <gkt@cs.luc.edu>
2021-09-25 23:59:14 -0500

    add comment to discourage running this script for no reason

--- setup-gtest.sh
+++ setup-gtest.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script is used to setup GoogleTest in GitHub Actions.
+# Unless you have a good reason to  run it, don't! :-)
+
 git clone https://github.com/google/googletest.git
 pushd googletest
 cmake .
