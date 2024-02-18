6884f7ae54945554cb9de3b09be37e8852ca3fe1
George K. Thiruvathukal <gkt@cs.luc.edu>
2024-02-17 21:40:08 -0600

    document the thought process of the two tests

--- std-queue/int_stack_tests.cc
+++ std-queue/int_stack_tests.cc
@@ -14,43 +14,52 @@ TEST(IntStackTests, Initialization)
     ASSERT_EQ(int_stack_capacity(&stack1), 10);
 }
 
-// This test fills the stack to capacity.
-// Then it attempts to fill the stack beyond capacity.
-// Does not check whether pushed values are valid.
-
 TEST(IntStackTests, PushToCapacityAndOverflow)
 {
+    // create stack that can hold 10 items
     int_stack_t stack1;
     const int capacity = 10;
     int_stack_init(&stack1, capacity);
+
+    // fill it
     for (int i=0; i < capacity; i++) {
         int result = int_stack_push(&stack1, i);
         ASSERT_TRUE(result);
     }
+
+    // ensure it's full
     ASSERT_EQ(int_stack_size(&stack1), capacity);
+
+    // try to add one more
     int result = int_stack_push(&stack1, capacity+1);
+
+    // and make sure it is an overflow (error)
     ASSERT_FALSE(result);
 }
 
-// This files the stack to capacity.
-// Then it pops items one at a time and checks that they are the expected values.
-// Then it attempts to pop from empty stack.
 
 TEST(IntStackTests, PushToCapcacityPopUntilUnderflow
 )
 {
+    // create stack that can hold 10 items [similar to previous test]
     int_stack_t stack1;
     const int capacity = 10;
     int_stack_init(&stack1, capacity);
+
+    // fill it
     for (int i=0; i < capacity; i++) {
         int result = int_stack_push(&stack1, i);
         ASSERT_TRUE(result);
     }
+
+    // now drain it one item at a time, ensuring each item is the value expected
     for (int i=capacity-1; i >= 0; i--) {
         int top_value;
         int result = int_stack_pop(&stack1, &top_value);
         ASSERT_EQ(i, top_value);
     }
+
+    // try to remove from empty stack and ensure it fails
     int top_value;
     int result = int_stack_pop(&stack1, &top_value);
     ASSERT_FALSE(result);
